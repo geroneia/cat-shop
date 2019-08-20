c5f314d74e59ec908fa1ac3d38292197ac44fe11
geroneia <kozetta2004@yandex.ru>
2019-08-20 15:57:51 +0300

    Вносит изменения в отображение мобильного меню, изображений для ретины и инпутов в форме

--- source/js/open-mobile-nav.js
+++ source/js/open-mobile-nav.js
@@ -2,6 +2,10 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+if (navMain.classList.contains('main-nav--opened')) {
+  navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+};
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
