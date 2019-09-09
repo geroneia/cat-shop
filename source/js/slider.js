a964920db7ce34d0389932e62c67abe98bf50c40
geroneia <kozetta2004@yandex.ru>
2019-09-09 15:32:56 +0300

    Заменяет блок .container на миксин .container() в index.html

--- source/js/slider.js
+++ source/js/slider.js
@@ -9,22 +9,22 @@ var finishBtn = instanceWrapperTab.querySelector('.range-slider__finish');
 var beforeImg = instanceWrapperTab.querySelector('.instance__photo-wrapper--fat-cat');
 var pin = instanceWrapperTab.querySelector('.range-slider__pin');
 var line = instanceWrapperTab.querySelector('.range-slider__scale');
-var setBackground = function () {
-  if (window.innerWidth>=TABLET_WIDTH && window.innerWidth<DESKTOP_WIDTH && instanceWrapperTab) {
-    instanceWrapperTab.style.backgroundImage = "linear-gradient(to left, #eaeaea " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px, #f2f2f2 " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px)";
-  }
-  if (window.innerWidth>=DESKTOP_WIDTH && instanceWrapperTab) {
-    instanceWrapperTab.style.backgroundImage = "linear-gradient(to left, #eaeaea " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px, #f2f2f2 " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px)";
-  }
-};
-setBackground();
+// var setBackground = function () {
+//   if (window.innerWidth>=TABLET_WIDTH && window.innerWidth<DESKTOP_WIDTH && instanceWrapperTab) {
+//     instanceWrapperTab.style.backgroundImage = "linear-gradient(to left, #eaeaea " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px, #f2f2f2 " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px)";
+//   }
+//   if (window.innerWidth>=DESKTOP_WIDTH && instanceWrapperTab) {
+//     instanceWrapperTab.style.backgroundImage = "linear-gradient(to left, #eaeaea " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px, #f2f2f2 " + (pin.getBoundingClientRect().left + PIN_WIDTH/2) + "px)";
+//   }
+// };
+// setBackground();
 startBtn.addEventListener('click', function () {
   beforeImg.classList.remove('instance__photo-wrapper--disappearance');
   beforeImg.classList.add('instance__photo-wrapper--appearance');
   pin.classList.remove('range-slider__pin--after');
   pin.classList.add('range-slider__pin--before');
   beforeImg.style.width = '100%';
-  setBackground();
+  // setBackground();
 });
 finishBtn.addEventListener('click', function () {
   beforeImg.classList.remove('instance__photo-wrapper--appearance');
@@ -32,7 +32,5 @@ finishBtn.addEventListener('click', function () {
   pin.classList.remove('.range-slider__pin--before');
   pin.classList.add('range-slider__pin--after');
   beforeImg.style.width = '0';
-  setBackground();
+  // setBackground();
 });
-
-
